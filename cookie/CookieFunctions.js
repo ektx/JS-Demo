af0260b4c9a1aeb93e8735d9d6c5aabfde7620c3
kings <530675800@qq.com>
2016-01-07 17:21:35 +0800

    归类时间处理

--- cookie/CookieFunctions.js
+++ cookie/CookieFunctions.js
@@ -1,4 +1,4 @@
-function setCookie(cookieName, cookieValue, cookiePath, cookieExprice) {
+﻿function setCookie(cookieName, cookieValue, cookiePath, cookieExprice) {
 	// 将非字母或数字字符串转换为 Latin-1 字符集中对应的十六进制编码，并加上%字符前缀
 	cookieValue = escape(cookieValue);
 
