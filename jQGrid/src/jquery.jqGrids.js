e6506d872e7280a06a0df93d4cdfe43e62c057d3
ektx <530675800@qq.com>
2016-09-12 17:10:14 +0800

    add new demo

--- jQGrid/src/jquery.jqGrids.js
+++ jQGrid/src/jquery.jqGrids.js
@@ -1027,136 +1027,6 @@ $.extend($.jgrid,{
 				icon_freeze : "ui-icon-grip-solid-vertical",
 				icon_move: "ui-icon-arrow-4"
 			}
-		},
-		Bootstrap : {
-			common : {
-				disabled: "ui-disabled",
-				highlight : "success",
-				hover : "active",
-				cornerall: "", 
-				cornertop: "",
-				cornerbottom : "",
-				hidden : "",
-				icon_base : "glyphicon",
-				overlay: "ui-overlay",
-				active : "active",
-				error : "bg-danger",
-				button : "btn btn-default",
-				content : ""
-			},
-			base : {
-				entrieBox : "",
-				viewBox : "table-responsive",
-				headerTable : "table table-bordered",
-				headerBox : "",
-				rowTable : "table table-bordered",
-				rowBox : "",
-				footerTable : "table table-bordered",
-				footerBox : "",
-				headerDiv : "",
-				gridtitleBox : "",
-				customtoolbarBox : "",
-				//overlayBox: "ui-overlay",
-				loadingBox : "row",
-				rownumBox :  "active",
-				scrollBox : "",
-				multiBox : "checkbox",
-				pagerBox : "",
-				pagerTable : "table",
-				toppagerBox : "",
-				pgInput : "form-control",
-				pgSelectBox : "form-control",
-				pgButtonBox : "",
-				icon_first : "glyphicon-step-backward",
-				icon_prev : "glyphicon-backward",
-				icon_next: "glyphicon-forward",
-				icon_end: "glyphicon-step-forward",
-				icon_asc : "glyphicon-triangle-top",
-				icon_desc : "glyphicon-triangle-bottom",
-				icon_caption_open : "glyphicon-circle-arrow-up",
-				icon_caption_close : "glyphicon-circle-arrow-down"
-			},
-			modal : {
-				modal : "modal-content",
-				header : "modal-header",
-				title : "modal-title",
-				content :"modal-body",
-				resizable : "ui-resizable-handle ui-resizable-se",
-				icon_close : "glyphicon-remove-circle",
-				icon_resizable : "glyphicon-import"
-			},
-			celledit : {
-				inputClass : 'form-control'
-			}, 
-			inlinedit : {
-				inputClass : 'form-control',
-				icon_edit_nav : "glyphicon-edit",
-				icon_add_nav : "glyphicon-plus",
-				icon_save_nav : "glyphicon-save",
-				icon_cancel_nav : "glyphicon-remove-circle"
-			},
-			formedit : {
-				inputClass : "form-control",
-				icon_prev : "glyphicon-step-backward",
-				icon_next : "glyphicon-step-forward",
-				icon_save : "glyphicon-save",
-				icon_close : "glyphicon-remove-circle",
-				icon_del : "glyphicon-trash",
-				icon_cancel : "glyphicon-remove-circle"
-			},
-			navigator : {
-				icon_edit_nav : "glyphicon-edit",
-				icon_add_nav : "glyphicon-plus",
-				icon_del_nav : "glyphicon-trash",
-				icon_search_nav : "glyphicon-search",
-				icon_refresh_nav : "glyphicon-refresh",
-				icon_view_nav : "glyphicon-info-sign",
-				icon_newbutton_nav : "glyphicon-new-window"
-			},
-			grouping : {
-				icon_plus : 'glyphicon-triangle-right',
-				icon_minus : 'glyphicon-triangle-bottom'
-			},
-			filter : {
-				table_widget : 'table table-condensed',
-				srSelect : 'form-control',
-				srInput : 'form-control',
-				menu_widget : '',
-				icon_search : 'glyphicon-search',
-				icon_reset : 'glyphicon-refresh',
-				icon_query :'glyphicon-comment'
-			},
-			subgrid : {
-				icon_plus : 'glyphicon-triangle-right',
-				icon_minus : 'glyphicon-triangle-bottom',
-				icon_open : 'glyphicon-indent-left'
-			},
-			treegrid : {
-				icon_plus : 'glyphicon-triangle-right',
-				icon_minus : 'glyphicon-triangle-bottom',
-				icon_leaf : 'glyphicon-unchecked'
-			},
-			fmatter : {
-				icon_edit : "glyphicon-edit",
-				icon_add : "glyphicon-plus",
-				icon_save : "glyphicon-save",
-				icon_cancel : "glyphicon-remove-circle",
-				icon_del : "glyphicon-trash"
-			},
-			colmenu : {
-				menu_widget : '',
-				input_checkbox : "",
-				filter_select: "form-control",
-				filter_input : "form-control",
-				icon_menu : "glyphicon-menu-hamburger",
-				icon_sort_asc : "glyphicon-sort-by-alphabet",
-				icon_sort_desc : "glyphicon-sort-by-alphabet-alt",
-				icon_columns : "glyphicon-list-alt",
-				icon_filter : "glyphicon-filter",
-				icon_group : "glyphicon-align-left",
-				icon_freeze : "glyphicon-object-align-horizontal",
-				icon_move: "glyphicon-move"
-			}
 		}
 	}
 });
@@ -6018,6 +5888,7 @@ $.extend($.jgrid,{
 	},
 	createModal : function(aIDs, content, p, insertSelector, posSelector, appendsel, css) {
 		p = $.extend(true, {}, $.jgrid.jqModal || {}, p);
+		console.log('Css: ', p)
 		var self = this,
 			rtlsup = $(p.gbox).attr("dir") === "rtl" ? true : false,
 			classes = $.jgrid.styleUI[(p.styleUI || 'jQueryUI')].modal,
@@ -6062,10 +5933,17 @@ $.extend($.jgrid,{
 		var coord = {};
 		if ( $.fn.jqm && p.jqModal === true) {
 			if(p.left ===0 && p.top===0 && p.overlay) {
-				var pos = [];
-				pos = $.jgrid.findPos(posSelector);
-				p.left = pos[0] + 4;
-				p.top = pos[1] + 4;
+				// var pos = [];
+				var _mw = $(mw);
+				var _mw_w = p.width || _mw.width()
+				var _mw_h = _mw.height()
+
+				console.log($(mw).width(), $(mw).height())
+
+				// pos = $.jgrid.findPos(posSelector);
+				p.left = (window.innerWidth - _mw_w)/2;
+				p.top = (window.innerHeight - _mw_h)/2;
+
 			}
 			coord.top = p.top+"px";
 			coord.left = p.left;
@@ -8011,7 +7889,6 @@ $.jgrid.extend({
 	},
 
 	searchGrid : function (p) {
-		debugger
 		var regional =  $.jgrid.getRegional(this[0], 'search');
 		p = $.extend(true, {
 			recreateFilter: false,
